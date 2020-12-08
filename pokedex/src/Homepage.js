02efe23f6a7ae313d1349a52398d02abb4907895
amoutai <amin.moutai@gmail.com>
2020-12-08 11:15:06 +0100

    Transforming the Homepage class into a Hooks

--- pokedex/src/Homepage.js
+++ pokedex/src/Homepage.js
@@ -1,62 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Homepage.css';
 import Search from './Search';
 import Card from './Card';
 import Container from './Container';
 
-class Homepage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            pokemon: {
-                name: '',
-                id: 0,
-                type: [],
-                image: ''
-            },
-            isLoading: false
-        }
-    }
+function Homepage() {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         pokemon: {
+    //             name: '',
+    //             id: 0,
+    //             type: [],
+    //             image: ''
+    //         },
+    //         isLoading: false
+    //     }
+    // }
 
-    fetchData(pokemon) {
-        fetch('https://pokeapi.co/api/v2/pokemon/' + pokemon)
+    const [pokemon, setPokemon] = useState({
+        name: '',
+        id: 0,
+        type: [],
+        image: ''
+    });
+
+    const [isLoading, setLoading] = useState(false);
+
+    let fetchData = (chosenPokemon) => {
+        fetch('https://pokeapi.co/api/v2/pokemon/' + chosenPokemon)
             .then(res => res.json())
             .then(res => {
-                this.setState({
-                    pokemon: {
-                        name: res.name,
-                        id: res.id,
-                        type: res.types.map(el => el.type.name),
-                        image: res.sprites.front_default
-                    },
-                }, () => {
-                    this.setState({ isLoading: true })
-                });
-                //console.log(res);
+                setPokemon({
+                    name: res.name,
+                    id: res.id,
+                    type: res.types.map(el => el.type.name),
+                    image: res.sprites.front_default
+                },
+                )
+                setLoading(true);
                 return res;
             })
             .catch(err => console.log('An error occurred : ' + err));
     }
 
-    render() {
-        return (
-            <main>
-                <div className="container">
-                    <div className="nav">
-                        <Search fetchData={this.fetchData.bind(this)} />
-                    </div>
-                    <div>
-                        <Card isLoading={this.state.isLoading} pokemon={this.state.pokemon} />
-                    </div>
-                    <div className="card-container">
-                        <Container className="highlight" title="Highlight" />
-                        <Container className="last-visited" title="Last visited" />
-                        <Container className="favorite" title="Favorites" />
-                    </div>
+    return (
+        <main>
+            <div className="container">
+                <div className="nav">
+                    <Search fetchData={fetchData.bind(this)} />
                 </div>
-            </main>
-        );
-    }
+                <div>
+                    <Card isLoading={isLoading} pokemon={pokemon} />
+                </div>
+                <div className="card-container">
+                    <Container className="highlight" title="Highlight" />
+                    <Container className="last-visited" title="Last visited" />
+                    <Container className="favorite" title="Favorites" />
+                </div>
+            </div>
+        </main>
+    )
 }
 
 export default Homepage;
