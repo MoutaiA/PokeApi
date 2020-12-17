c4cc7746ebee5338ac7450d736e7839fc6b9a259
amoutai <amin.moutai@gmail.com>
2020-12-17 10:48:49 +0100

    Customing the 404 page

--- pokedex/src/NotFound.js
+++ pokedex/src/NotFound.js
@@ -3,10 +3,15 @@ import './NotFound.css';
 
 class NotFound extends React.Component {
     render() {
-        return(
-        <div>
-            <h1>Error 404 : The page can't be found !</h1>
-        </div>
+        return (
+            <div className="error-404">
+                <h1>Error 404 : The page can't be found !</h1>
+                <div className="error-content">
+                    <div>We are sorry !</div>
+                    <div>If you want to comeback to the Homepage, cou can click <a href="/">here</a></div>
+                </div>
+                <img src="images/error.png" alt="Psyduck error 404" />
+            </div>
         );
     }
 }
